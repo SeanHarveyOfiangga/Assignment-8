d67a4e0c34dbb56d919b047c4f1da39a110b08e6
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-12-15 13:01:23 +0800

    created winning combination function

--- Lottery.py
+++ Lottery.py
@@ -7,11 +7,14 @@
 #If the user enter “y” the user will play again
 #if “n” the program will exit.
 
+import random
+
+
 def intro():
     print("\n\033[47m\033[30m**Welcome to Lottery App**\033[0m")
 intro()
 
-def userbet():
+def lottobet():
     print("""\n\033[01m0-9 LOTTO BETTING INSTRUCTION:\033[0m
     \033[01m\033[36m>>\033[0m Enter your \033[32mthree number combination\033[0m from \033[04m0-9\033[0m as your bet
     \033[01m\033[36m>>\033[0m Winners will receive \033[93m5,000,000 PHP\033[0m
@@ -26,9 +29,17 @@ def userbet():
             continue
         if bet >= 10:
             print("\033[91mYour bet is over the limit, please enter your bet again.\n\033[0m")
-            continue
         if bet not in combination:
-            combination.append(bet)    
+            combination.append(bet)
+        else:
+            print("\033[91mOops! You already picked this number.\n\033[0m")
+            continue 
         entry += 1
-    print(combination)
-userbet()
+    return combination
+
+def winningcombi():
+    win = []
+    combi = random.sample(range(0,9),3)
+    win.append(combi)
+    return win
+
