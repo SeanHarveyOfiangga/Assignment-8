ba189fb1bff2262d84a05b5f357564401c808c25
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-12-15 14:46:28 +0800

    Created intruction for the program

---
+++ GuessNumber.py
@@ -0,0 +1,22 @@
+#Program 2: Guess the number
+#Generate 1 random number (0-100)
+#Ask the user to guess the number
+#Display “Greater than” if the inputed number is greater than the random number
+#Display “Less than” if the inputed number is less than the random number
+#Repeat asking the user until the random number has been guessed correctly.
+
+import random
+
+def intro():
+    print("\n\033[47m\033[30m**Welcome to Guess the number app!**\033[0m")
+intro()
+
+def number():
+    print("""\n\033[01mHow to play Guess the number?\033[0m
+    \033[01m\033[36m>>\033[0m \033[33mRandom number from \033[04m0-100\033[0m will be generated and the user will guess what is the number.\033[0m
+    \033[01m\033[36m>>\033[0m \033[33mThe program will say if the guess is higher or lower than the unknown number.\033[0m
+    \033[01m\033[36m>>\033[0m \033[33mIf the guess matched the unknown number, user will be declared as the winner.\033[0m
+    *********************************************************************************""")
+    
+
+number()
